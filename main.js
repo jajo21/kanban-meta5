6cc101fec64096b320c83c10383be1965d6b4181
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-10-19 15:41:38 +0200

    Tog bort kod som var till en obefintlig fil

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // import av alla moduels
 import { logOut } from "./modules/logout.mjs";
 import { addCard } from "./modules/add-card.mjs";
-import { dragDrop } from "./modules/drag-drop.mjs";
 
 const logOutBtn = document.querySelector(".logOutButton")
 
@@ -10,4 +9,3 @@ logOutBtn.addEventListener("click", function (){
 });
 
 addCard();
-dragDrop();
